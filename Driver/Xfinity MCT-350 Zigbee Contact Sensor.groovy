daab6c62d626ff3c753a47758cddf6d8df280a89
almulder <albertjmulder@gmail.com>
2024-06-20 11:16:31 -0600

    Update Xfinity MCT-350 Zigbee Contact Sensor.groovy

--- Driver/Xfinity MCT-350 Zigbee Contact Sensor.groovy
+++ Driver/Xfinity MCT-350 Zigbee Contact Sensor.groovy
@@ -1,3 +1,4 @@
+
 /**
 *  Xfinity MCT-350 Zigbee Contact Sensor
 *
@@ -14,7 +15,7 @@
 *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *  for the specific language governing permissions an limitations under the License.
 *
-*
+*  Version: 1.0
 */
 
 metadata {
